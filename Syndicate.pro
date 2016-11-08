989d83df9a903219b918369a215954e706f4110a
Syndicate <development@syndicatelabs.org>
2016-11-07 17:32:52 -0800

    Version bump
    Version bump

--- Syndicate.pro
+++ Syndicate.pro
@@ -1,6 +1,6 @@
 TARGET = Syndicate
 TEMPLATE = app
-VERSION = 1.0.1.7
+VERSION = 1.0.1.8
 
 greaterThan(QT_MAJOR_VERSION, 5) {
     INCLUDEPATH += src src/json src/qt src/qt/plugins/mrichtexteditor
--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Syndicate");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-Beta"
+#define CLIENT_VERSION_SUFFIX   ""
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
